bb561b94220deda322c306e195ccb7d3035c68fa
Atyom Timofeev <timofeev.a.vch@gmail.com>
2023-05-17 11:41:14 +0500

    Обработка смены языка

--- SQRT/main/static/main/js/script.js
+++ SQRT/main/static/main/js/script.js
@@ -1,32 +1,45 @@
-function calculateRoot() {
-  const input = document.getElementById("number-input").value;
-  const regex = /^(-?\d+(\.\d+)?)([-+])(\d+(\.\d+)?)i$/;
-  const match = input.match(regex);
+var english = document.getElementById("en"),
+    russian = document.getElementById("ru"),
+    headerMainChangeText = document.getElementById("headerMainChangeText"),
+    headerSupportChangeText = document.getElementById("headerSupportChangeText"),
+    inputNumberChangeText = document.getElementById("inputNumberChangeText"),
+    calcBtnChangeText = document.getElementById("calcBtnChangeText"), 
+    ourContactsChangeText = document.getElementById("ourContactsChangeText");
 
-  if (match) {
-    const real = parseFloat(match[1]);
-    const imag = parseFloat(match[4]);
-    const sign = match[3];
+english.addEventListener("click", function() {
+    change(english, russian);
+  }, false
+);
 
-    if (sign === "+") {
-      const complex = math.complex(real, imag);
-      const root = math.sqrt(complex);
-      const result = `√${input} = <span>${root.toString()}</span>`;
-      document.getElementById("result").innerHTML = result;
-    } else {
-      const result = "Введите корректное комплексное число!";
-      document.getElementById("result").textContent = result;
-    }
-  } else {
-    const number = parseFloat(input);
+russian.addEventListener("click", function() {
+    change(russian, english);
+  }, false
+);
 
-    if (!isNaN(number)) {
-      const root = math.sqrt(number);
-      const result = `√${number} = <span>${root}</span>`;
-      document.getElementById("result").innerHTML = result;
-    } else {
-      const result = "Введите корректное число!";
-      document.getElementById("result").textContent = result;
-    }
+function change(lang_on, lang_off1, lang_off2) {
+  if (!lang_on.classList.contains("current_lang")) {
+    lang_on.classList.add("current_lang");
+    lang_off1.classList.remove("current_lang");
+    lang_off2.classList.remove("current_lang");
+  }
+
+  if (lang_on.innerHTML == "EN") {
+    change_text.classList.add("english");
+    change_text.classList.remove("russian");
+    headerMainChangeText.innerHTML = "Main",
+    headerSupportChangeText.innerHTML = "Support",
+    inputNumberChangeText.innerHTML = "Input number:",
+    calcBtnChangeText.innerHTML = "Calculate",
+    ourContactsChangeText.innerHTML = "Our contacts: brigada@mail.ru";
+  }
+  
+  else {
+    change_text.classList.add("russian");
+    change_text.classList.remove("english");
+    headerMainChangeText.innerHTML = "Главная",
+    headerSupportChangeText.innerHTML = "Поддержка",
+    inputNumberChangeText.innerHTML = "Введите число:",
+    calcBtnChangeText.innerHTML = "Вычислить",
+    ourContactsChangeText.innerHTML = "Наши контакты: bridaga@mail.ru";
   }
 }
